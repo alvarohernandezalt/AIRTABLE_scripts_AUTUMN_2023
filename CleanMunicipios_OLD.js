c3006fc4872dcbf13b5cda3c01106adb9caf376b
alvarohernandezalt <107269919+alvarohernandezalt@users.noreply.github.com>
2023-11-18 22:09:01 +0100

    Initial commit

---
+++ CleanMunicipios_OLD.js
@@ -0,0 +1,26 @@
+
+// Get the table from Airtable
+let table = base.getTable('YourTableName');
+
+// Fetch all records from the table
+let records = await table.selectRecordsAsync();
+
+// Iterate over each record
+for (let record of records.records) {
+  // Get the value of the MUNICIPIO column
+  let municipio = record.getCellValue('MUNICIPIO');
+
+  // Get the value of the DIRECCION column
+  let direccion = record.getCellValue('DIRECCION');
+
+  // Find the index of the first "-" character in the direccion value
+  let index = direccion.indexOf('-');
+
+  // Insert the municipio value after the first "-" character with a space between them
+  let newDireccion = direccion.substring(0, index + 1) + ' ' + municipio + direccion.substring(index + 1);
+
+  // Update the DIRECCION column with the new value
+  table.updateRecordAsync(record, {
+    'DIRECCION': newDireccion
+  });
+}
