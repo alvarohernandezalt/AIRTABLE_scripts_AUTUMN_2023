fa7cfc479481ff01e1044c1c58ad33e882b62be0
alvarohernandezalt <107269919+alvarohernandezalt@users.noreply.github.com>
2023-11-21 18:16:31 +0100

    Update

--- BAT/sELECT_ORDER_HowManyMore.bat
+++ BAT/sELECT_ORDER_HowManyMore.bat
@@ -4,11 +4,13 @@ setlocal enabledelayedexpansion
 set "folderPath=%~dp0"
 
 for /R "%folderPath%" %%F in ("*.*") do (
-    for /f "delims=" %%I in ('dir /b /od "%%~nF*%%~xF"') do set "latestFile=%%I"
-    for %%O in ("%%~nF*%%~xF") do (
-        if not "%%O"=="!latestFile!" (
-            echo Deleting: %%O
-            del "%%O"
+    if /I not "%%~xF"==".bat" (
+        for /f "delims=" %%I in ('dir /b /od "%%~nF*%%~xF"') do set "latestFile=%%I"
+        for %%O in ("%%~nF*%%~xF") do (
+            if not "%%O"=="!latestFile!" (
+                echo Deleting: %%O
+                del "%%O"
+            )
         )
     )
 )
