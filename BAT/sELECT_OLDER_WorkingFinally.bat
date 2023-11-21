45a3ce1672da4a9b01ad45279663880e949c71c6
alvarohernandezalt <107269919+alvarohernandezalt@users.noreply.github.com>
2023-11-21 18:18:51 +0100

    Create sELECT_OLDER_WorkingFinally.bat

---
+++ BAT/sELECT_OLDER_WorkingFinally.bat
@@ -0,0 +1,19 @@
+@echo off
+setlocal enabledelayedexpansion
+
+set "folderPath=%~dp0"
+
+for /R "%folderPath%" %%F in ("*.*") do (
+    if /I not "%%~xF"==".bat" (
+        for /f "delims=" %%I in ('dir /b /od "%%~nF*%%~xF"') do set "latestFile=%%I"
+        for %%O in ("%%~nF*%%~xF") do (
+            if not "%%O"=="!latestFile!" (
+                echo Deleting: %%O
+                del "%%O"
+            )
+        )
+    )
+)
+
+endlocal
+exit
