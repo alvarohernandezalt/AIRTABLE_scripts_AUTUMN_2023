5fbcf659baafc429a748616499b2b55706243802
alvarohernandezalt <107269919+alvarohernandezalt@users.noreply.github.com>
2023-11-23 12:42:22 +0100

    Update Update_COM_DOC_lastPresupuesto.js

--- JS/Update_COM_DOC_lastPresupuesto.js
+++ JS/Update_COM_DOC_lastPresupuesto.js
@@ -1,40 +1,48 @@
-/**
- * This script updates the records in the "PRESUPUESTOS" table based on the input values.
- * It filters the records based on the provided "numeroPresupuesto" value and finds the record with the highest "ID_PRESUPUESTO" value.
- * Then, it updates the "FECHA_ENTREGA_PREVISTA", "COMENTARIO_ESPECIFICO", and "DOCUMENTACION_ESPACIFICA" columns with the input values.
- */
-
+/* This script updates a record in the "PRESUPUESTOS" table based on the input values. 
+It filters the records based on the "idMatriz" field, finds the record with the highest "ID_SECUNDARIA" value, 
+and updates the "FECHA_ENTREGA_PREVISTA", "COMENTARIO_ESPECIFICO", and "DOC_ESPECIFICA" fields 
+with the corresponding values from the found record.
+*/
 let table = base.getTable("PRESUPUESTOS");
 let query = await table.selectRecordsAsync();
 
 // Get the input values
 let inputValues = input.config();
 let numero_pre = inputValues.numeroPresupuesto;
-let nuevaFechaEntrega = inputValues.nuevaFechaEntrega;
-let comentarioSeguimimento = inputValues.comentarioSeguimimento;
-let documentacionAnexa = inputValues.documentacionAnexa;
+
 
 // Filter the records based on the input values
 let filteredRecords = query.records.filter(record => {
     return record.getCellValue("idMatriz") === numero_pre;
 });
-
+console.log(filteredRecords);
 // Find the record with the highest ID_PRESUPUESTO value
 let maxId = 0;
 let maxRecord = null;
 filteredRecords.forEach(record => {
-    let idPresupuesto = record.getCellValue("ID_PRESUPUESTO");
-    if (idPresupuesto > maxId) {
+    let idPresupuesto = record.getCellValue("ID_SECUNDARIA");
+    if (idPresupuesto > maxId && record.id !== inputValues.recordID) {
         maxId = idPresupuesto;
         maxRecord = record;
     }
 });
+console.log(maxRecord);
+let nuevaFechaEntrega = maxRecord !== null ? maxRecord.getCellValue("nuevaFECHA_ENTREGA") : null;
+let comentarioSeguimimento = maxRecord !== null ? maxRecord.getCellValue("COMENTARIO_SEGUIMIENTO") : null;
+let documentacionAnexa = maxRecord !== null ? maxRecord.getCellValue("DOCUMENTACION_ANEXA") : null;
+console.log(nuevaFechaEntrega,comentarioSeguimimento,documentacionAnexa);
 
 // Update the FECHA_ENTREGA_PREVISTA column with the input value
 if (maxRecord !== null) {
-    await table.updateRecordAsync(maxRecord.id, {
-        "FECHA_ENTREGA_PREVISTA": nuevaFechaEntrega,
-        "COMENTARIO_ESPECIFICO": comentarioSeguimimento,
-        "DOCUMENTACION_ESPACIFICA": documentacionAnexa
-    });
+    const updateFields = {};
+    if (nuevaFechaEntrega !== null) {
+        updateFields["FECHA_ENTREGA_PREVISTA"] = nuevaFechaEntrega;
+    }
+    if (comentarioSeguimimento !== null) {
+        updateFields["COMENTARIO_ESPECIFICO"] = comentarioSeguimimento;
+    }
+    if (documentacionAnexa !== null) {
+        updateFields["DOC_ESPECIFICA"] = documentacionAnexa;
+    }
+    await table.updateRecordAsync(inputValues.recordID, updateFields);
 }
