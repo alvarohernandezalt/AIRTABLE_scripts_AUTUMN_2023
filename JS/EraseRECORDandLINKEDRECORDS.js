07b2ce0a95d9a16c8c6ef9dd706ed14e4a314b58
alvarohernandezalt <107269919+alvarohernandezalt@users.noreply.github.com>
2023-11-27 15:28:03 +0100

    Update EraseRECORDandLINKEDRECORDS.js

--- JS/EraseRECORDandLINKEDRECORDS.js
+++ JS/EraseRECORDandLINKEDRECORDS.js
@@ -1,10 +1,17 @@
-let table = base.getTable("HOJA_RECOGIDA_PRESUPUESTOS");
-let linkedTable = base.getTable("PRESUPUESTOS");
+/*
+This code deletes a record and its linked records from two tables in a database. 
+It first retrieves the record ID from the input configuration. 
+Then, it selects the record from the "PRINCIPAL_TABLE" and checks if it has any linked records. 
+If there are linked records, it retrieves their IDs and deletes them from the "LINKED_TABLE". 
+Finally, it deletes the original record from the "PRINCIPAL_TABLE".
+*/
+let table = base.getTable("PRINCIPAL_TABLE");
+let linkedTable = base.getTable("LINKED_TABLE");
 let inputConfig = input.config();
 let recordId = inputConfig.recordId;
 
 let record = await table.selectRecordsAsync();
-let linkedRecords = record.records.filter(r => r.id === recordId)[0].getCellValue('PRESUPUESTOS');
+let linkedRecords = record.records.filter(r => r.id === recordId)[0].getCellValue('PRINCIPAL_TABLE_LINKED_FIELD');
 
 if (linkedRecords) {
     let linkedRecordIds = linkedRecords.map(lr => lr.id);
