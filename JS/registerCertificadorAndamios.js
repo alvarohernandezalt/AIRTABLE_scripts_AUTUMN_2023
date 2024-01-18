5ce03205b8d3553a7c23f59119ee58e2480f5d7c
alvarohernandezalt <107269919+alvarohernandezalt@users.noreply.github.com>
2024-01-18 09:12:27 +0100

    Update registerCertificadorAndamios.js

--- JS/registerCertificadorAndamios.js
+++ JS/registerCertificadorAndamios.js
@@ -6,27 +6,17 @@ let obraRowId = inputValues.obraRowId;
 
 let table = base.getTable('OBRAS_ACTIVAS');
 
+let query = {
+    filterByFormula: `{ROWID} = '${obraRowId}'`
+};
 
-// Search for the record with matching obraRowId
-let query = await table.selectRecordsAsync({
-    filterByFormula: `ROWID() = '${obraRowId}'`
-});
+table.selectRecordsAsync(query).then(result => {
+    let matchingRecords = result.records.filter(record => record.getCellValue('ROWID').includes(obraRowId));
+    console.log(matchingRecords[0].id)});
 
-if (query.records.length > 0) {
-    let record = query.records[0];
-    let certificadorAndamioField = record.getCellValue('CERTIFICADOR_ANDAMIO');
+// Convert certificadorRowId to a string with comma separation
+let certificadorRowIdString = certificadorRowId.join(',');
 
-    // Append the certificadorRowId to the existing values in CERTIFICADOR_ANDAMIO field
-    if (certificadorAndamioField) {
-        certificadorAndamioField += `, ${certificadorRowId}`;
-    } else {
-        certificadorAndamioField = certificadorRowId;
-    }
-
-    // Update the record with the new value in CERTIFICADOR_ANDAMIO field
-    await table.updateRecordAsync(record, {
-        'CERTIFICADOR_ANDAMIO': certificadorAndamioField
-    });
-}
-
+output.set('certificadorRowIdString', certificadorRowIdString);
+output.set('newObraId', matchingRecords[0].id )
 
