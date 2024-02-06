2155ca7ded8189a33f19b348ebfc12c37a25658b
alvarohernandezalt <107269919+alvarohernandezalt@users.noreply.github.com>
2024-02-06 19:44:56 +0100

    Update otherPivotTable.js

---
+++ JS/otherPivotTable.js
@@ -0,0 +1,36 @@
+let table = base.getTable('filtradoTodo');
+let view = table.getView('2023');
+let queryResult = await view.selectRecordsAsync();
+
+// Assuming 'pivotLookUp' is linked to a table called 'Months'
+let monthsTable = base.getTable('Months');
+let monthsQueryResult = await monthsTable.selectRecordsAsync();
+
+// Create a mapping from month abbreviations to full names
+let monthMapping = {
+    'DIC': 'DICIEMBRE',
+    'NOV': 'NOVIEMBRE',
+    'OCT': 'OCTUBRE',
+    'SEP': 'SEPTIEMBRE',
+    'AUG': 'AGOSTO',
+    'JUL': 'JULIO',
+    'JUN': 'JUNIO',
+    'MAY': 'MAYO',
+    'APR': 'ABRIL',
+    'MAR': 'MARZO',
+    'FEB': 'FEBRERO',
+    'JAN': 'ENERO'
+};
+
+for (let record of queryResult.records) {
+    let mesEntrada = record.getCellValue('mesEntrada');
+    if (monthMapping.hasOwnProperty(mesEntrada)) {
+        let monthName = monthMapping[mesEntrada];
+        let monthRecord = monthsQueryResult.records.find(record => record.name === monthName);
+        if (monthRecord) {
+            await table.updateRecordAsync(record, {
+                'pivotLookUp': [{id: monthRecord.id}]
+            });
+        }
+    }
+}
