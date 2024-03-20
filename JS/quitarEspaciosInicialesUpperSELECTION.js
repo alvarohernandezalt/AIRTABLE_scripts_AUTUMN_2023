2fcdfb1f68eb4b614d76feb34be084e05a591e13
alvarohernandezalt <107269919+alvarohernandezalt@users.noreply.github.com>
2024-03-20 14:33:02 +0100

    Update quitarEspaciosInicialesUpperSELECTION.js

--- JS/quitarEspaciosInicialesUpperSELECTION.js
+++ JS/quitarEspaciosInicialesUpperSELECTION.js
@@ -1,19 +1,23 @@
 // Get the table
 let table = base.getTable('MATRICES_BASE');
 
+// Ask the user to pick the source and destination fields
+let sourceField = await input.fieldAsync("Campo Fuente", table);
+let destinationField = await input.fieldAsync("Campo Destino", table);
+
 // Query the records in the table
 let query = await table.selectRecordsAsync();
 
 // Iterate over each record
 for (let record of query.records) {
-    // Get the 'NOMBRE ADMOR' field value, trim it, and convert it to uppercase
-    let nombreAdmor = record.getCellValue('NOMBRE ADMOR');
-    if (nombreAdmor) {
-        nombreAdmor = nombreAdmor.trim().toUpperCase();
+    // Get the source field value, trim it, and convert it to uppercase
+    let fieldValue = record.getCellValue(sourceField.name);
+    if (fieldValue) {
+        fieldValue = fieldValue.trim().toUpperCase();
 
-        // Update the 'nombreADMINISTRADOR' field with the new string
+        // Update the destination field with the new string
         await table.updateRecordAsync(record, {
-            'nombreADMINISTRADOR': nombreAdmor
+            [destinationField.name]: fieldValue
         });
     }
 }
